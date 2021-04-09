dfa5f8ad82fe1bf295fd8beeb1fb525c6233bf56
Chris-Curry <chriscurry070199@gmail.com>
2021-04-09 15:28:47 +0100

    changed

---
+++ webroot/js/scripts.js
@@ -0,0 +1,20 @@
+function clearForm() {
+    window.confirm("Are you sure you want to clear the text?");
+}
+
+function validateForm() {
+    var subButton = document.getElementById('submitButton');
+    var inputTitle = document.getElementById('title');
+    var inputContent = document.getElementById('blogContent');
+
+    blogForm.addEventListener('click', function(event) {
+        if (inputTitle.value === '' || blogContent.value === '') {
+            title.setAttribute("style", "box-shadow: 0 0 10px red;");
+            inputContent.setAttribute("style", "box-shadow: 0 0 10px red;");
+            event.preventDefault();
+        } else {
+
+        }
+
+    }, false)
+}
