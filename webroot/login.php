9300d837e4904ef4154c7d43de93af3a7a05ab2f
Chris-Curry <chriscurry070199@gmail.com>
2021-04-09 17:03:16 +0100

    changed

---
+++ webroot/login.php
@@ -0,0 +1,23 @@
+<?php 
+
+require("conn.php");
+
+
+
+if (isset($_POST['submit'])){
+    $email = $_POST['email'];
+    $password = $_POST ['password'];
+    $query = "SELECT * FROM `users` WHERE email = '$email' 'AND password = '$password'";
+    
+    $result = mysqli_query($conn,$query);
+
+    if (mysqli_num_rows($result) >= 0){
+        session_start();
+        echo("Login Successful");
+    }
+    else{
+        die("Login Failed");
+    }
+}
+
+?>
