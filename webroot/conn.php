3f2b9a727c257cbb8f88de350652a362b941a6ff
Chris-Curry <chriscurry070199@gmail.com>
2021-04-09 15:36:59 +0100

    changed

--- webroot/conn.php
+++ webroot/conn.php
@@ -1,3 +1,4 @@
+<?php
 $dbhost = getenv("MYSQL_SERVICE_HOST");
 $dbport = getenv("MYSQL_SERVICE_PORT");
 $dbuser = getenv("DATABASE_USER");
@@ -11,3 +12,4 @@ $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 if ($conn->connect_error) {
  die("Connection failed: " . $conn->connect_error);
 }
+?>
