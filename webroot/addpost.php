66a52045df3b70a436c9c98f1a8f2bdbbe377ee7
Chris-Curry <chriscurry070199@gmail.com>
2021-04-12 15:39:06 +0100

    changed

--- webroot/addpost.php
+++ webroot/addpost.php
@@ -3,14 +3,14 @@ require("conn.php");
 session_start();
 
 if (isset($_POST['submit'])){
-    $title = $_POST['title'];
-    $text = $_POST['text'];
+    $title = $_POST['blogTitle'];
+    $text = $_POST['blogText'];
     $date = date("d-m-y");
 
     $sql = "INSERT INTO BLOGS (title, text, time)
     VALUES ('$title', '$text', '$date')";
     mysqli_query($conn,$sql);
-    
+
     echo("Blog posted Successful");
     header("location:viewblog.html");
 }
